0b0ee7f04ad8cfc18d007738e4b3e748b75eeed0
Shreyansh Khaitan <shrey14112002@gmail.com>
2024-04-09 22:56:50 +0530

    initial

---
+++ Navigate/script.js
@@ -0,0 +1,46 @@
+function initAutocomplete() {
+    var fromInput = document.getElementById('from');
+    var toInput = document.getElementById('to');
+
+    var autocompleteFrom = new google.maps.places.Autocomplete(fromInput);
+    var autocompleteTo = new google.maps.places.Autocomplete(toInput);
+}
+
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var from = document.getElementById('from').value;
+    var to = document.getElementById('to').value;
+
+    // You can perform further actions with the 'from' and 'to' values here
+    console.log('From:', from);
+    console.log('To:', to);
+});
+
+document.getElementById('submitBtn').addEventListener('click', function() {
+    // Redirect to map.html
+    window.location.href = './Maps/map.html';
+});
+
+
+// document.getElementById('submitBtn').addEventListener('click', function() {
+//     var from = document.getElementById('from').value;
+//     var to = document.getElementById('to').value;
+
+//     if (from === 'none' || to === 'none') {
+//         alert('Please select valid locations');
+//         return;
+//     }
+
+//     var mapUrl = 'https://www.google.com/maps/embed/v1/directions?key=YOUR_API_KEY&origin=' + encodeURIComponent(from) + '&destination=' + encodeURIComponent(to);
+    
+//     var iframe = document.createElement('iframe');
+//     iframe.src = mapUrl;
+//     iframe.setAttribute('width', '100%');
+//     iframe.setAttribute('height', '500');
+//     iframe.setAttribute('frameborder', '0');
+//     iframe.setAttribute('allowfullscreen', '');
+    
+//     var mapDiv = document.getElementById('mapDiv');
+//     mapDiv.innerHTML = '';
+//     mapDiv.appendChild(iframe);
+// });
+
---
+++ script.js
@@ -0,0 +1,15 @@
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  this.classList.toggle('fa-eye-slash');
+  this.classList.toggle('fa-eye');
+});
+
+document.getElementById('loginButton').addEventListener('click', function() {
+    // Redirect to the new page
+    window.location.href = './Navigate/navigate.html';
+  });
+  
